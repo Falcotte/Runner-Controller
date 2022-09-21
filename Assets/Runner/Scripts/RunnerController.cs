b262bbc4aaca3f8d39b4f9c9b4cf30d826c8382d
Ali Unal <0033484@gmail.com>
2022-09-21 13:36:45 +0300

    Forward movement added

--- Assets/Runner/Scripts/RunnerController.cs
+++ Assets/Runner/Scripts/RunnerController.cs
@@ -7,6 +7,7 @@ namespace AngryKoala.RunnerControls
     {
         [SerializeField] private Transform visual;
 
+        [SerializeField] private float forwardSpeed;    // Percentage of distance traveled per second
         [SerializeField] private float horizontalSpeed;
 
         [SerializeField] private float horizontalInputSensitivity;
@@ -14,33 +15,56 @@ namespace AngryKoala.RunnerControls
 
         [SerializeField] private float maxPlayerRotation;
 
+        private float currentTraveledDistance;
+
         private float targetXPos;
 
         private bool isMoving;
         public bool IsMoving => isMoving;
 
+        private bool hasReachedEnd;
+
         private void OnEnable()
         {
+            InputManager.Instance.InputAreas[0].OnTouch += StartMovement;
             InputManager.Instance.InputAreas[0].OnDragDeltaScreenPercent += MoveHorizontal;
         }
 
         private void OnDisable()
         {
+            InputManager.Instance.InputAreas[0].OnTouch -= StartMovement;
             InputManager.Instance.InputAreas[0].OnDragDeltaScreenPercent -= MoveHorizontal;
-        }
-
-        private void Start()
-        {
-            isMoving = true;
         }
 
         private void Update()
         {
             if(isMoving)
             {
+                currentTraveledDistance += forwardSpeed * Time.deltaTime;
+                transform.position = Vector3.Lerp(RunnerLevel.Instance.RunnerStartTransform.position, RunnerLevel.Instance.RunnerEndTransform.position, currentTraveledDistance / 100f);
+
+                if(currentTraveledDistance >= 100f)
+                {
+                    currentTraveledDistance = 100f;
+                    StopMovement();
+                }
+
                 visual.localPosition = new Vector3(Mathf.Lerp(visual.localPosition.x, targetXPos, horizontalSpeed * Time.deltaTime), visual.localPosition.y, visual.localPosition.z);
                 visual.localRotation = Quaternion.Euler(new Vector3(0f, (visual.localPosition.x - targetXPos) * -maxPlayerRotation / (Mathf.Abs(horizontalPositionLimits.x) + Mathf.Abs(horizontalPositionLimits.y)), 0f));
             }
+        }
+
+        private void StartMovement()
+        {
+            if(!hasReachedEnd)
+            {
+                isMoving = true;
+            }
+        }
+
+        private void StopMovement()
+        {
+            isMoving = false;
         }
 
         private void MoveHorizontal(Vector2 dragDeltaPercent)
